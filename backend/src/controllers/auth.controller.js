e8cc1f68561bae760b1bb758167584eeeb94a692
Albert Yosia <albertyosia77@gmail.com>
2025-04-07 22:10:07 +0800

    Add profile page

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -94,17 +94,16 @@ export const updateProfile = async (req, res) => {
     const userId = req.user._id;
 
     if (!profilePic) {
-      return res.send(400).json({ message: 'Profile pic is required' });
+      return res.status(400).json({ message: 'Profile pic is required' });
     }
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
-    const updatedUser = User.findByIdAndUpdate(
+    const updatedUser = await User.findByIdAndUpdate(
       userId,
       { profilePic: uploadResponse.secure_url },
       { new: true }
     );
-
-    res.send(200).json(updatedUser);
+    res.status(200).json(updatedUser);
   } catch (error) {
     console.log('Error in update profile: ', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
